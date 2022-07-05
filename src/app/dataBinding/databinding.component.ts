7f036f3bb4cf163d46427ce6d983c4c931f415bc
prashant4110 <pk.pupri@gmail.com>
2022-07-05 10:39:06 +0530

    data binding

---
+++ src/app/dataBinding/databinding.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'databinding',
+    templateUrl:'./databinding.component.html'
+})
+export class DataBinding {
+    public name:string;
+    public count:number;
+    public ctrlstring:string;
+    constructor(){
+        this.name="prashant gupta";
+        this.count=10;
+        this.ctrlstring="total count"
+    }
+
+    // default acces modifier is always public
+
+    inc(evct:any){
+        this.count++;
+        console.log(evct);
+        
+    }
+
+}
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: "hello",
+    template:`
+        <div>
+            <h4>Hello Angullar</h4>
+            <p>this is a simple ng component cretated manually</p>
+        </div>
+    `
+})
+export class HelloComponent{
+
+} 
