f79a594981b4121951091f23f4806a1dd57f6921
prashant4110 <pk.pupri@gmail.com>
2022-07-05 11:59:52 +0530

    2way binding with hidden div

--- src/app/dataBinding/databinding.component.ts
+++ src/app/dataBinding/databinding.component.ts
@@ -9,10 +9,12 @@ export class DataBinding {
     public name:string;
     public count:number;
     public ctrlstring:string;
+    public isvisible:boolean=false;
     constructor(){
         this.name="prashant gupta";
         this.count=10;
         this.ctrlstring="total count"
+        // this.isvisible=false;
     }
 
     // default acces modifier is always public
